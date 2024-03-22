26ccaa3e1c1de34c570e228b8a09abf218d50c3f
keenalevine <keenalevine97@gmail.com>
2024-03-22 13:29:22 +0800

    style: trend

---
+++ apps/analysis/app/(routes)/(route)/new/(dashboard)/overview/trend/page.tsx
@@ -0,0 +1,7 @@
+import DashboardContentWrapper from '@app/_features/dashboard/DashboardContentWrapper';
+
+const TrendPage = () => {
+  return <DashboardContentWrapper>{/*<Trend />*/}</DashboardContentWrapper>;
+};
+
+export default TrendPage;
