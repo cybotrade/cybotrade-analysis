1902b3b01fa34a500eb8f05e37efd55a7e043410
keenalevine <keenalevine97@gmail.com>
2024-03-25 16:04:37 +0800

    chore: hide modal temporary

--- apps/analysis/app/_features/dashboard/right/ActionHeader.tsx
+++ apps/analysis/app/_features/dashboard/right/ActionHeader.tsx
@@ -10,10 +10,11 @@ export const ActionHeader = ({ className }: ActionHeaderProps) => {
     <div className={cn(className, 'font-sora flex justify-between items-center h-10')}>
       <ToggleSwitch />
       <div className="flex items-center justify-end gap-5">
-        <WidgetsModal
-          button={<ChangeIcon className="w-5 h-auto text-[#7B7878] cursor-pointer" />}
-          content={<WidgetsModalContent />}
-        />
+        <ChangeIcon className="w-5 h-auto text-[#7B7878] cursor-pointer" />
+        {/*<WidgetsModal*/}
+        {/*  button={<ChangeIcon className="w-5 h-auto text-[#7B7878] cursor-pointer" />}*/}
+        {/*  content={<WidgetsModalContent />}*/}
+        {/*/>*/}
         <BackSquareIcon className="w-5 h-auto text-[#7B7878] cursor-pointer" />
       </div>
     </div>
