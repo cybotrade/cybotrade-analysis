1e66615f58953ee88a97afc7fb7cc3c4fddb197d
keenalevine <keenalevine97@gmail.com>
2024-05-21 15:40:02 +0800

    fix: replace app router error handling instead of react-error-boundary

--- apps/analysis/app/(routes)/error.tsx
+++ apps/analysis/app/(routes)/error.tsx
@@ -2,16 +2,14 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 
-type ErrorPageProps = {
+export default function Error({
+  error,
+  reset,
+}: {
   error: Error & { digest?: string };
   reset: () => void;
-};
-
-export const Error: React.FC<ErrorPageProps> = ({ error, reset }) => {
-  const router = useRouter();
-
+}) {
   return (
     <div className="h-screen w-full flex flex-col justify-center items-center gap-5">
       <Image src="/images/Cable.png" width={80} height={80} alt="cable" />
@@ -23,13 +21,11 @@ export const Error: React.FC<ErrorPageProps> = ({ error, reset }) => {
       </div>
       <Link
         href="/"
-        rel="noopener noreferrer"
         replace
-        onClick={() => reset()}
         className="font-sans text-black font-bold bg-primary px-6 py-2 flex justify-center items-center rounded-lg hover:opacity-80 w-[330px] h-[60px]"
       >
         Back to Home
       </Link>
     </div>
   );
-};
+}
--- apps/analysis/app/_providers/client.tsx
+++ apps/analysis/app/_providers/client.tsx
@@ -5,10 +5,8 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import gsap from 'gsap';
 import { Suspense, useState } from 'react';
-import { ErrorBoundary } from 'react-error-boundary';
 import { Toaster } from 'react-hot-toast';
 
-import { Error } from '@app/_components/error/error';
 import { getQueryClient } from '@app/_utils/queryClient';
 
 import ThemeProvider from './theme';
@@ -23,25 +21,19 @@ export const ClientProvider: React.FC<ClientProviderProps> = ({ children }) => {
   const queryClient = getQueryClient();
 
   return (
-    <ErrorBoundary
-      fallbackRender={({ error, resetErrorBoundary }) => (
-        <Error error={error} reset={resetErrorBoundary} />
-      )}
-    >
-      <Suspense>
-        <QueryClientProvider client={queryClient}>
-          <ThemeProvider
-            attribute="class"
-            // defaultTheme="system"
-            defaultTheme="light"
-            enableSystem={false}
-          >
-            <Toaster />
-            {children}
-            <ReactQueryDevtools />
-          </ThemeProvider>
-        </QueryClientProvider>
-      </Suspense>
-    </ErrorBoundary>
+    <Suspense>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider
+          attribute="class"
+          // defaultTheme="system"
+          defaultTheme="light"
+          enableSystem={false}
+        >
+          <Toaster />
+          {children}
+          <ReactQueryDevtools />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </Suspense>
   );
 };
